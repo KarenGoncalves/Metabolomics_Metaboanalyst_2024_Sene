57e22c2509be989a55dec1ab3a74df4bf6029204
KarenGoncalves <cris.kgs@gmail.com>
2024-05-30 16:12:15 -0400

    renamed plots

--- scripts/MetaboAnalyst_MultiVariate_Analyses.R
+++ scripts/MetaboAnalyst_MultiVariate_Analyses.R
@@ -31,37 +31,40 @@ for (i in 1:3) {
     
     mSet<-PlotPCAPairSummary(
         mSet, pc.num = 3, 
-        imgName = paste0(plot_basename[[i]], "_pca_pair_0_"),
+        imgName = paste0(plot_basename[[i]], "PCA_pair_0_"),
         format = "png", dpi = 300, width=NA)
     
     # Create PCA scree plot
     mSet<-PlotPCAScree(
         mSet, 
-        imgName = paste0(plot_basename[[i]], "_pca_scree_0_"),
-        format = "png", dpi = 72, width=NA, 5)
+        imgName = paste0(plot_basename[[i]], "PCA_scree_0_"),
+        format = "png", dpi = 72, width=NA, scree.num = 5)
     
     # Create a 2D PCA score plot
     mSet<-PlotPCA2DScore(
         mSet,
-        imgName = paste0(plot_basename[[i]], "_pca_score2d_0_"), 
+        imgName = paste0(plot_basename[[i]], "PCA_score2d_0_"), 
         format = "png", dpi=72, 
-        width=NA, 1, 2, 0.95, 1, 0)
+        width=NA, pcx = 1, pcy = 2, reg = 0.95, 
+        show = 0, grey.scale = 0)
 
     #### Partial least square - discriminant analysis (PLS-DA) ####
     
-    mSet = runPLS.Analysis (mSetObj = mSet, 
-                     compNum = 8,
-                     iterations = 10000, 
-                     method = "oscorespls",  
-                     basenamePlots = plot_basename[[i]])
-    plot_RegCoef(mSet, 1)
+    mSet = runPLS.Analysis(
+        mSetObj = mSet, 
+        compNum = 8,
+        iterations = 10000, 
+        method = "oscorespls",  
+        basenamePlots = plot_basename[[i]])
+    # plot_RegCoef(mSet, 1)
     
     #### OPLS-DA ####
     mSet = OPLSR.Anal(mSetObj = mSet, reg = T)    
     mSet = OPLSDA.Permut(mSetObj = mSet, num = 1000)
     # Create a 2D oPLS-DA score plot
     mSet<-PlotOPLS2DScore(mSet, 
-                          imgName = paste0(plot_basename[[i]], "_opls_score2d_0_"), 
+                          imgName = paste0(plot_basename[[i]], 
+                                           "OPLS_score2d_0_"), 
                           format = "png", 
                           dpi=72, 
                           width=NA, 
@@ -71,11 +74,14 @@ for (i in 1:3) {
                           grey.scale = 0)
     
     # Create a significant features plot
-    # mSet<-PlotOPLS.Splot(mSet, "opls_splot_0_", "all", "png", 72, width=NA);
+    mSet<-PlotOPLS.Splot(mSet, 
+                         imgName = paste0(plot_basename[[i]], 
+                                          "OPLS_splot_0_"), 
+                         "all", "png", 72, width=NA);
     # 
     # # Create a plot of features ranked by importance
-    # mSet<-PlotOPLS.Imp(mSet, "opls_imp_0_", "png", 72, width=NA, "vip", "tscore", 15,FALSE)
+    # mSet<-PlotOPLS.Imp(mSet, "OPLS_imp_0_", "png", 72, width=NA, "vip", "tscore", 15,FALSE)
     # 
     # # Create a plot of the model overview
-    # mSet<-PlotOPLS.MDL(mSet, "opls_mdl_0_", format = "png", dpi=72, width=NA)
+    # mSet<-PlotOPLS.MDL(mSet, "OPLS_mdl_0_", format = "png", dpi=72, width=NA)
 }
