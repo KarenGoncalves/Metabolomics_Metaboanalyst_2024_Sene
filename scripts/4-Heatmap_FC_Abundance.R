7f56ee0bfc7e05b956e822ec7e592cb73035cea5
KarenGoncalves <cris.kgs@gmail.com>
2024-06-06 15:29:34 -0400

    heatmps of fold change and z-score

--- scripts/4-Heatmap_FC_Abundance.R
+++ scripts/4-Heatmap_FC_Abundance.R
@@ -8,6 +8,10 @@ Analysis_modes = c("HILIC_Positive",
                    "RP_Positive",
                    "RP_Negative")
 inFiles <- paste0("Results/", Analysis_modes, "_mSet.RData")
+mets_abundance = sapply(inFiles, simplify = F, \(x) {
+    load(x)
+    mSet$dataSet$filt %>% as.data.frame
+})
 plot_basename <- paste0("plots/", Analysis_modes, "/")
 tableContrast <- 
     data.frame(Numerator = c("AC9.1", "AC9.2", "AC9.3",
@@ -22,6 +26,8 @@ FC_threshold = 1
 #                   log2FC, plotContrast,
 #                   AnalysisMode)
 load("Results/Siggenes_DAAs.RData")
+
+#### Plot differential abundance ####
 
 og_diff_abundance <- 
     differential_abundance %>%
@@ -83,22 +89,26 @@ list("Original" = og_diff_abundance,
     })
 dev.off()
 
-DAMs_abundance <- sapply(met_abundance, simplify = F, \(x) {
-    (t(x) %>% as.data.frame())[1:12]
+#### Plot peak normalized peak height ####
+DAMs_abundance <- sapply(mets_abundance %>% names(), 
+                         simplify = F, \(x) {
+    (t(mets_abundance[[x]]) %>% as.data.frame())[1:12] %>%
+        as.data.frame() %>%
+        mutate(Analysis_mode = gsub("_mSet.RData",
+                                    "", basename(x))
+        )
 }) %>% list_rbind() %>% 
-    filter(rownames(.) %in% order_mets$labels) %>%
-    mutate(Metabolite = factor(rownames(.),
-                               levels = order_mets$labels)) %>%
-    pivot_longer(cols = !Metabolite,
+    filter(rownames(.) %in% og_diff_abundance$Metabolite) %>%
+    mutate(Metabolite = rownames(.)) %>%
+    pivot_longer(cols = -c(Metabolite, Analysis_mode),
                  names_to="Replicate",
-                 values_to="Norm_abundance") %>%
-    mutate(Clone = gsub("_I+$", "", Replicate)) %>%
-    filter(Metabolite %in% order_mets$labels) 
+                 values_to="Height") %>%
+    mutate(Clone = gsub("_I+$", "", Replicate)) 
 
 summary_DAMs <- DAMs_abundance %>%
-    group_by(Metabolite) %>%
-    summarize(Mean_norm_abundance = mean(Norm_abundance, na.rm=T),
-              SD_norm_abundance = sd(Norm_abundance, na.rm=T))
+    group_by(Metabolite, Analysis_mode) %>%
+    summarize(Mean_norm_abundance = mean(Height, na.rm=T),
+              SD_norm_abundance = sd(Height, na.rm=T))
 
 zscore_DAMs <- 
     sapply(1:nrow(DAMs_abundance), simplify = F, \(x) {
@@ -106,41 +116,80 @@ zscore_DAMs <-
         info = summary_DAMs %>%
             filter(Metabolite == Met)
         DAMs_abundance[x,] %>%
-            mutate(zScore = (Norm_abundance - info$Mean_norm_abundance) /
+            mutate(zScore = (Height - info$Mean_norm_abundance) /
                        info$SD_norm_abundance)
     }) %>% list_rbind()
 
 
-reorder_mets <- (zscore_DAMs %>% 
-                     dplyr::select(Metabolite, Replicate, zScore) %>%
-                     pivot_wider(id_cols = Metabolite,
-                                 names_from = Replicate,
-                                 values_from = zScore) %>%
-                     data.frame(row.names = .$Metabolite))[-1] %>%
+reorder_mets <- 
+    sapply(Analysis_modes, simplify = F, \(x) {
+        (data = zscore_DAMs %>%
+            filter(Analysis_mode == x) %>%
+            dplyr::select(Metabolite, Replicate, zScore) %>%
+            pivot_wider(id_cols = Metabolite,
+                        names_from = Replicate,
+                        values_from = zScore) %>%
+            data.frame(row.names = .$Metabolite))[-1] %>%
     dist() %>% hclust()
+    })
+mets_per_AM = sapply(Analysis_modes, \(x) length(reorder_mets[[x]]$order))
+Mets_ordered = data.frame(
+    Metabolite = sapply(reorder_mets, \(x) x$labels) %>%
+        unlist %>%
+        unname(),
+    order = c(reorder_mets[[1]]$order,
+              reorder_mets[[2]]$order + mets_per_AM[1],
+              reorder_mets[[3]]$order + mets_per_AM[1] + mets_per_AM[2]
+    )
+) %>% arrange(order)
+nDAMs = nrow(Mets_ordered)
 
-
-nDAMs = length(reorder_mets$order)
-zscore_DAMs %>%
+zscore_plots <- 
+    sapply(Analysis_modes, simplify = F, \(x) {
+        zscore_DAMs %>%
+            filter(Analysis_mode == x) %>%
     mutate(ordered_Mets = Metabolite %>%
-               factor(levels = reorder_mets$labels[reorder_mets$order])) %>%
-    ggplot(aes(ordered_Mets, y = Replicate, fill = Norm_abundance)) +
+               factor(levels = reorder_mets[[x]]$labels[
+                   reorder_mets[[x]]$order])
+           )  %>%
+    ggplot(aes(ordered_Mets, y = Replicate, fill = zScore)) +
     geom_tile() +
     scale_fill_gradientn(colors = rev(brewer.pal(11, "RdBu")), 
                          limits = c(-4, 4),
                          breaks = seq(-4, 4, 2),
                          labels = seq(-4, 4, 2)) +
-    labs(x = "", y="", fill="Normalized\nabundance",
-         caption=paste("Normalized abundance of deregulated analytes (all three analysis modes).
-                       Deregulated analytes:", nDAMs)) +
-    facet_wrap(~Clone, nrow = 4, drop = T, scales = "free_y") +
+    labs(x = "", y="", fill="Peak intensity\n(z-score)",
+         title = gsub("_", " ", x)) +
+    facet_grid(rows = vars(Clone), 
+               drop = T, scales = "free_y") +
     theme_classic() +
     theme(legend.position = "bottom",
           axis.text.x = element_blank(),
           axis.ticks.x = element_blank(),
           strip.background = element_blank(),
-          strip.text.x = element_blank(), 
-          panel.spacing.y = unit(-2, "mm"))
+          strip.text.y = element_blank(), 
+          panel.spacing = unit(-2, "mm"),
+          plot.margin = margin(r=-.2,
+                               t=0, b=0, l=-.3,
+                               'cm'))
+})
+
+library(ggpubr)
+zscore_plots[2:3] <- sapply(
+    zscore_plots[2:3], simplify = F, 
+    \(x) {
+        x + rremove('y.ticks') +
+            rremove('y.axis') +
+            rremove('y.text')
+})
+rel_widths = (mets_per_AM/sum(mets_per_AM)) + 
+    c(0.1, 0, 0)
+ggarrange(plotlist = zscore_plots, 
+          vjust = T, common.legend = T,
+          legend = "bottom", 
+          ncol = 3, 
+          widths = rel_widths[1]
+)
 
 ggsave("plots/Relative_abundance_DAAs.pdf", 
        height = 6, width = 8)
