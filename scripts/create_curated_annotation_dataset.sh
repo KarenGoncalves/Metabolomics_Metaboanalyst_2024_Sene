6befa6cd0144e485741091408dacb07c7bbe6662
KarenGoncalves <cris.kgs@gmail.com>
2024-06-10 14:25:35 -0400

    curating annotation

--- scripts/create_curated_annotation_dataset.sh
+++ scripts/create_curated_annotation_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 get_annotation (){
- tail -n +5 1 | cut -f 1-35 > $2
+ tail -n +5 $1 | cut -f 1-35 > $2
 }
 
 #Positive
@@ -11,7 +11,7 @@ get_annotation "${input}" ${output}
 
 
 ##Negative
-input="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_RP_Negativee_Identification_MSDial_format.txt"
+input="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_RP_Negative_Identification_MSDial_format.txt"
 output="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/Corrected_LCMSMS_RP_Negative_identification.txt"
 get_annotation "${input}" ${output}
 
