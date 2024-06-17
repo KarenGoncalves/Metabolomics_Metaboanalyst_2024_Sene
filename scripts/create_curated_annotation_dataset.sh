2d65e0693385018d44a5736bc9b6b1eed9d073c5
KarenGoncalves <cris.kgs@gmail.com>
2024-06-17 11:12:37 -0400

    added if block to change disk depending on computer

--- scripts/create_curated_annotation_dataset.sh
+++ scripts/create_curated_annotation_dataset.sh
@@ -4,19 +4,25 @@ get_annotation (){
  tail -n +5 $1 | cut -f 1-35 > $2
 }
 
+if [ -d "/e/Metabolomics_MetaboAnalyst_2024_Sene" ]; then
+  disk=/e
+else
+  disk=/d
+fi
+
 #Positive
-input="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_RP_Positive_Identification_MSDial_format.txt"
-output="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/Corrected_LCMSMS_RP_Positive_identification.txt"
+input="${disk}/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_RP_Positive_Identification_MSDial_format.txt"
+output="${disk}/Metabolomics_Metaboanalyst_2024_Sene/Inputs/Corrected_LCMSMS_RP_Positive_identification.txt"
 get_annotation "${input}" ${output}
 
 
 ##Negative
-input="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_RP_Negative_Identification_MSDial_format.txt"
-output="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/Corrected_LCMSMS_RP_Negative_identification.txt"
+input="${disk}/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_RP_Negative_Identification_MSDial_format.txt"
+output="${disk}/Metabolomics_Metaboanalyst_2024_Sene/Inputs/Corrected_LCMSMS_RP_Negative_identification.txt"
 get_annotation "${input}" ${output}
 
 
 ##HILIC
-input="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_HILIC_Positive_Identification_MSDial_format.txt"
-output="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/Corrected_LCMSMS_HILIC_Positive_identification.txt"
+input="${disk}/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_HILIC_Positive_Identification_MSDial_format.txt"
+output="${disk}/Metabolomics_Metaboanalyst_2024_Sene/Inputs/Corrected_LCMSMS_HILIC_Positive_identification.txt"
 get_annotation "${input}" ${output}
