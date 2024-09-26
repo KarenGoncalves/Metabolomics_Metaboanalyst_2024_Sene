fc2a3778f56b55e45a30829f5d17a2b6d43c2663
KarenGoncalves <cris.kgs@gmail.com>
2024-09-26 13:05:36 -0400

    automating creation of metaboanalyst dataset

--- scripts/create_MetaboAnalyst_dataset.sh
+++ scripts/create_MetaboAnalyst_dataset.sh
@@ -4,9 +4,12 @@
 #mkdir /e/Metabolomics_Metaboanalyst_2024_Sene/metadata
 #mkdir /e/Metabolomics_Metaboanalyst_2024_Sene/scripts
 
-#Positive has 17 samples: 12 replicates, 2 QCs, 3 blanks
-input=/e/Samples\ and\ QCs_Positives/Height_0_2024_05_23_10_09_51.txt
-output="/e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_RP_Positive_rawHeight.txt"
+create_dataset () {
+col_start=$1
+input=$2
+output=$3
+col_end=$(awk 'END {print NF}' ${input})
+
 head -n5 "${input}" |\
  tail -n 1 | cut -f 36-52 > tmp_header
 
@@ -14,7 +17,7 @@ cat tmp_header | sed -E 's/([A-Z0-9\.]+)([\._])([I12]+)\t/\1\t/g' |\
  sed -E 's/([A-Za-z]+\.blank[\.a-z]*)/BLANK/g' > tmp_row2
 
 tail -n +6 "${input}" |\
- cut -f 36-52 > tmp_Raw_height
+ cut -f ${col_start}-${col_end} > tmp_Raw_height
 
 cat tmp_header tmp_row2 tmp_Raw_height > tmp_file
 
@@ -25,46 +28,14 @@ awk 'BEGIN {FS="\t";OFS="/"; print "Sample\nLabel"}\
 paste tmp_RowNames tmp_file > ${output}
 
 rm tmp_file tmp_RowNames tmp_Raw_height tmp_row2 tmp_header
+}
 
-##Negative has 17 samples: 12 replicates, 2 QCs, 3 blanks
-input=/e/Samples\ and\ QCs_Negative/Height_0_2024_05_23_09_56_49.txt
+#Positive 
 
-head -n5 "${input}" |\
- tail -n 1 | cut -f 36-52 > tmp_header
+create_dataset 32 RAW_DATA/LCMSMS_RP_Positive_MSDial.txt Inputs/LCMSMS_RP_Positive_rawHeight.txt
 
-cat tmp_header | sed -E 's/([A-Z0-9\.]+)([\._])([I12]+)\t/\1\t/g' |\
- sed -E 's/([A-Za-z]+\.blank[\.a-z]*)/BLANK/g' > tmp_row2
+##Negative 
+create_dataset 32 RAW_DATA/LCMSMS_RP_Negative_MSDial.txt Inputs/LCMSMS_RP_Negative_rawHeight.txt
 
-tail -n +6 "${input}" |\
- cut -f 36-52 > tmp_Raw_height
-
-cat tmp_header tmp_row2 tmp_Raw_height > tmp_file
-
-awk 'BEGIN {FS="\t";OFS="/"; print "Sample\nLabel"}\
- NR > 5 {print $2,$3}' "${input}"\
- > tmp_RowNames
-
-paste tmp_RowNames tmp_file > /e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_RP_Negative_rawHeight.txt
-rm tmp_file tmp_RowNames tmp_Raw_height tmp_row2 tmp_header
-
-##HILIC has 16 samples: 12 replicates, 2 QCs, 2 blanks
-input=/e/Samples\ and\ QCs\ recalibrated_HILIC/Height_0_2024_05_04_11_47_27.txt
-
-head -n5 "${input}" |\
- tail -n 1 | cut -f 36-51 > tmp_header
-
-cat tmp_header | sed -E 's/([A-Z0-9\.]+)([\._])([I12]+)\t/\1\t/g' |\
- sed -E 's/([A-Za-z]+\.blank[\.a-z]*)/BLANK/g' > tmp_row2
-
-tail -n +6 "${input}" |\
- cut -f 36-51 > tmp_Raw_height
-
-cat tmp_header tmp_row2 tmp_Raw_height > tmp_file
-
-awk 'BEGIN {FS="\t";OFS="/"; print "Sample\nLabel"}\
- NR > 5 {print $2,$3}' "${input}"\
- > tmp_RowNames
-
-paste tmp_RowNames tmp_file > /e/Metabolomics_Metaboanalyst_2024_Sene/Inputs/LCMSMS_HILIC_Positive_rawHeight.txt
-
-rm tmp_file tmp_RowNames tmp_Raw_height tmp_row2 tmp_header
+##HILIC 
+create_dataset 32 RAW_DATA/LCMSMS_HILIC_Positive_MSDial.txt Inputs/LCMSMS_HILIC_Positive_rawHeight.txt
